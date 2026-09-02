762034301a85de971172e23ff7f6edb167bedb6e
Dusk-92 <romain.thomas92@proton.me>
2026-09-02 23:45:52 +0200

    build: use Zig 0.16 winapi calling convention

--- experiments/wp24b-gc/main.zig
+++ experiments/wp24b-gc/main.zig
@@ -11,7 +11,7 @@
 
 const std = @import("std");
 const hook = @import("zhook");
-const WINAPI = std.os.windows.WINAPI;
+const WINAPI = std.builtin.CallingConvention.winapi;
 
 const LUA_COLLECT_GARBAGE_ADDR: usize = 0x6F7340;
 const LUA_CLOSE_ADDR: usize = 0x6F6EF0;
